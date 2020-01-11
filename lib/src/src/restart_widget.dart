2f1c0d8fd509a98b5bd0637ea5f2431de4f6fae4
Tomasz Polański <polanski.tomek@gmail.com>
2020-01-11 13:17:50 +0100

    Safer types

--- lib/src/src/restart_widget.dart
+++ lib/src/src/restart_widget.dart
@@ -1,35 +1,31 @@
-import 'package:fast_flutter_driver/src/src/tests.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rxdart/rxdart.dart';
 
-class RestartWidget extends StatefulWidget {
+class RestartWidget<T> extends StatefulWidget {
   const RestartWidget({
     @required this.builder,
-    this.initial,
     Key key,
   }) : super(key: key);
 
-  final BaseConfiguration initial;
-  final Widget Function(BuildContext, BaseConfiguration) builder;
+  final Widget Function(BuildContext, T) builder;
 
-  static Future<void> restartApp<T extends BaseConfiguration>(
-      BaseConfiguration configuration) {
+  static Future<void> restartApp<T>(T configuration) {
     final state = _RestartWidgetState.global.currentContext
         .findAncestorStateOfType<_RestartWidgetState>();
     return state.restartApp(configuration);
   }
 
   @override
-  _RestartWidgetState createState() => _RestartWidgetState();
+  _RestartWidgetState<T> createState() => _RestartWidgetState<T>();
 }
 
-class _RestartWidgetState extends State<RestartWidget> {
+class _RestartWidgetState<T> extends State<RestartWidget<T>> {
   bool _restarting = false;
-  BaseConfiguration _configuration;
+  T _configuration;
   static final global = GlobalKey<_RestartWidgetState>();
 
-  Future<void> restartApp(BaseConfiguration configuration) async {
+  Future<void> restartApp(T configuration) async {
     setState(() {
       _restarting = true;
       _configuration = configuration;
@@ -45,9 +41,9 @@ class _RestartWidgetState extends State<RestartWidget> {
       key: global,
       child: _restarting
           ? const SizedBox()
-          : (_configuration ?? widget.initial) == null
+          : _configuration == null
               ? _StartingTests()
-              : widget.builder(context, _configuration ?? widget.initial),
+              : widget.builder(context, _configuration),
     );
   }
 }
