d15952b91c810872003b836b9474c3350f00c25b
Tomek Polański <polanski.tomek@gmail.com>
2020-07-05 19:34:39 +0200

    Fixing test coverage (#47)
    * Fixing test coverage
    
    * Update version_test.dart
    
    * Update all_tests.dart
    
    Co-authored-by: Tomek Polanski <tpolanski@groupon.com>

--- tool/test/all_tests.dart
+++ tool/test/all_tests.dart
@@ -1,9 +1,13 @@
 import 'main_test.dart' as main_test;
 import 'preparing_tests/commands_test.dart' as commands_test;
 import 'preparing_tests/testing_test.dart' as testing_test;
+import 'update/version_test.dart' as version_test;
+import 'utils/enum_test.dart' as enum_test;
 
 void main() {
   main_test.main();
   commands_test.main();
   testing_test.main();
+  version_test.main();
+  enum_test.main();
 }
