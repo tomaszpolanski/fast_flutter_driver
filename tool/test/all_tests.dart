594703fa36e40278fcd516731a35af02ad663d2d
Tomek Polanski <tpolanski@groupon.com>
2020-07-05 11:11:17 +0200

    WIP

---
+++ tool/test/all_tests.dart
@@ -0,0 +1,9 @@
+import 'main_test.dart' as main_test;
+import 'preparing_tests/commands_test.dart' as commands_test;
+import 'preparing_tests/testing_test.dart' as testing_test;
+
+void main() {
+  main_test.main();
+  commands_test.main();
+  testing_test.main();
+}
