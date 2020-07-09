ff6df2fbce32f7adbd679652a7dd83299ce528e5
Tomek Polański <polanski.tomek@gmail.com>
2020-07-09 08:16:19 +0200

    Update path_provider_impl.dart

--- tool/lib/src/update/path_provider_impl.dart
+++ tool/lib/src/update/path_provider_impl.dart
@@ -6,7 +6,6 @@ import 'package:path/path.dart';
 
 // coverage:ignore-start
 class PathProvider {
-  String get scriptDir =>
-      dirname(Platform.script.toFilePath()); // coverage:ignore-line
+  String get scriptDir => dirname(Platform.script.toFilePath());
 }
 // coverage:ignore-end
