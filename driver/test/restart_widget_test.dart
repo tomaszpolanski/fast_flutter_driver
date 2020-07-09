3663acc681184aea18b0dc5337e3b80adc8042a8
Tomek Polański <polanski.tomek@gmail.com>
2020-07-09 08:50:09 +0200

    More tests (#64)

--- driver/test/restart_widget_test.dart
+++ driver/test/restart_widget_test.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:fast_flutter_driver/driver.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
