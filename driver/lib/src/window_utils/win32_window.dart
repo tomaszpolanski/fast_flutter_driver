374b7a20c12935ef8497292a6de9a384f18f0ae8
Tomek Polanski <tpolanski@groupon.com>
2021-03-23 09:46:17 +0100

    Fixing changing resolution on windows

--- driver/lib/src/window_utils/win32_window.dart
+++ driver/lib/src/window_utils/win32_window.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:fast_flutter_driver/src/window_utils/window_utils.dart';
 import 'package:flutter/material.dart';
 
 class Win32Window implements SystemWindow {
   @override
   Future<bool> setSize(Size size) {
-    return Future.value(false);
+    appWindow
+      ..size = size
+      ..show();
+    return Future.value(true);
   }
 }
